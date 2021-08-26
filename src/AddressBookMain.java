afe76de207728bf2188671660a89e94df83d30ce
Vinay288 <vinayhiremath288@gmail.com>
2021-08-26 21:57:45 +0530

    ADDED method for addition edition and deletion of conatct details

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
 
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-		
-			System.out.println("Welcome to Address Book Program");
+		Scanner s = new Scanner(System.in);
+		System.out.println("Welcome to Address Book Program");
+		AddressBook addressBook = new AddressBook();
+		for (;;) {
+			System.out.println("1.add contact\n2.show contact\n3.edit contact\n4.delete\n5.exit");
+			int value = s.nextInt();
+			if (value == 5)
+				break;
+			switch (value) {
+			case 1:
+				addressBook.addContact();
+				break;
+			case 2:
+				addressBook.showContacts();
+				break;
+			case 3:addressBook.editContact();
+					break;
+			case 4:
+				addressBook.deleteContact();
+				break;
+			}
 
-		
+		}
+		System.out.println("exited");
+
 	}
 
 }
--- src/Contact.java
+++ src/Contact.java
@@ -1,16 +1,88 @@
 
 public class Contact {
-	String firstName,lastName,address,city,state,email;
-	int zip,phoneNumber;
-	Contact(String firstName, String lastName,String address,String city,String state,int zip,int phoneNumber,String email){
-		this.firstName=firstName;
-		this.lastName=lastName;
-		this.address=address;
-		this.city=city;
-		this.state=state;
-		this.zip=zip;
-		this.phoneNumber=phoneNumber;
-		this.email=email;
+	private String firstName, lastName, address, city, state, email;
+	private int zip, phoneNumber;
+
+	Contact(String firstName, String lastName, String address, String city, String state, int zip, int phoneNumber,
+			String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
 	}
-	
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getZipCode() {
+		return zip;
+	}
+
+	public void setZipCode(int zip) {
+		this.zip = zip;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmailId() {
+		return email;
+	}
+
+	public void setEmailId(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return getFirstName() + "\n" + getLastName() + "\n" + getAddress() + "\n" + getPhoneNumber() + "\n"
+				+ getEmailId();
+	}
+
 }

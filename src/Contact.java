0a9849c05a7b0cc4ce7e6c9a9065876d3d188654
Vinay288 <vinayhiremath288@gmail.com>
2021-08-26 20:35:16 +0530

    ADDED: contact creation feature

--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,16 @@
 
 public class Contact {
-
+	String firstName,lastName,address,city,state,email;
+	int zip,phoneNumber;
+	Contact(String firstName, String lastName,String address,String city,String state,int zip,int phoneNumber,String email){
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.address=address;
+		this.city=city;
+		this.state=state;
+		this.zip=zip;
+		this.phoneNumber=phoneNumber;
+		this.email=email;
+	}
+	
 }

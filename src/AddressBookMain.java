d11eb579191da4ae054787ecb2b0cc9f81c50b90
Vinay288 <vinayhiremath288@gmail.com>
2021-08-27 20:20:06 +0530

    ADDED:feature to create a contact in addressbook

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -6,27 +6,6 @@ public class AddressBookMain {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Welcome to Address Book Program");
 		AddressBook addressBook = new AddressBook();
-		for (;;) {
-			System.out.println("1.add contact\n2.show contact\n3.edit contact\n4.delete\n5.exit");
-			int value = s.nextInt();
-			if (value == 5)
-				break;
-			switch (value) {
-			case 1:
-				addressBook.addContact();
-				break;
-			case 2:
-				addressBook.showContacts();
-				break;
-			case 3:addressBook.editContact();
-					break;
-			case 4:
-				addressBook.deleteContact();
-				break;
-			}
-
-		}
-		System.out.println("exited");
 
 	}
 

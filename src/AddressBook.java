315f55c328a43e43845bc224c08172e68e6b6d2c
Vinay288 <vinayhiremath288@gmail.com>
2021-08-27 20:43:08 +0530

    ADDED:feature to edit contacts

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -3,6 +3,8 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 
+
+
 public class AddressBook {
 
 	Scanner s = new Scanner(System.in);
@@ -38,6 +40,68 @@ public class AddressBook {
 			System.out.println(contactBook[i]);
 		}
 	}
+	public void editContact() {
+		System.out.println("Edit contact:");
+		System.out.println("Select Option:\n1.First Name\n2.Last Name\n3.City\n4.State\n5.Zip Code\n6.Phone\n7.Email");
+		int choice = s.nextInt();
+		System.out.println("Enter First Name of contact to be edited");
+		String editName = s.next();
+		int index=0;
+		for(index=0;index<numberOfConatcts;index++) {
+			if(contactBook[index].getFirstName().equals(editName)) {
+				break;
+			}
+			else {
+				System.out.println("no such conatact found");
+				return;
+			}
+		}
+		switch (choice) {
+		case 1:
+			System.out.println("Enter new First Name");
+			String newFName = s.next();
+			contactBook[index].setFirstName(newFName);
+			System.out.println("Edited");
+			break;
+		case 2:
+			System.out.println("Enter new Last Name");
+			String newLName = s.next();
+			contactBook[index].setLastName(newLName);
+			System.out.println("Edited");
+			break;
+		case 3:
+			System.out.println("Enter new City");
+			String newCity = s.next();
+			contactBook[index].setCity(newCity);
+			System.out.println("Edited");
+			break;
+		case 4:
+			System.out.println("Enter new State");
+			String newState = s.next();
+			contactBook[index].setState(newState);
+			System.out.println("Edited");
+			break;
+		case 5:
+			System.out.println("Enter new State");
+			int newZip = s.nextInt();
+			contactBook[index].setZipCode(newZip);
+			System.out.println("Edited");
+			break;
+		case 6:
+			System.out.println("Enter new Phone Number");
+			int newPNumber = s.nextInt();
+			contactBook[index].setPhoneNumber(newPNumber);
+			System.out.println("Edited");
+			break;
+		case 7:
+			System.out.println("Enter new Email");
+			String newEmail = s.next();
+			contactBook[index].setEmailId(newEmail);
+			System.out.println("Edited");
+			break;
+		}
+	}
+
 	
 
 }
--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -7,6 +7,7 @@ public class AddressBookMain {
 		System.out.println("Welcome to Address Book Program");
 		AddressBook addressBook = new AddressBook();
 		addressBook.addContact();
+		addressBook.editContact();
 		addressBook.showContacts();
 
 	}

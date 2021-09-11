cf570f3504027eb70be93660389bb2b764d7d634
Vinay288 <vinayhiremath288@gmail.com>
2021-09-11 23:29:51 +0530

    MODIFY: fucntion call to search method

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class AddressBookMain {
+	static String addressBookName = "";
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program");
@@ -9,50 +10,70 @@ public class AddressBookMain {
 	}
 
 	public static void menu(AddressBookArray addressBooks) {
-		AddressBookImpl addressOperation = new AddressBookImpl();
-		String addressBookName = "";
+
 		Scanner s = new Scanner(System.in);
 		for (;;) {
-			for (;;) {
-				System.out.println(
-						"1.create new address book\n2.edit existing address book\n3.show address books\n4.exit");
+			System.out.println(
+					"1.create new address book\n2.edit existing address book\n3.show address books\n4.search\n5.exit");
 
-				int choice = s.nextInt();
-				if (choice == 1) {
-					addressBooks.addAddressBooks();
-				} else if (choice == 2) {
-					System.out.println("enter Contact book name");
-					addressBookName = s.next();
-					if (addressBooks.selectAddressBook(addressBookName) != null)
-						break;
-				} else if (choice == 3) {
-					addressBooks.showAddressBook();
-				} else {
-					System.out.println("Bye!!");
-					return;
-				}
-			}
-
-			System.out.println("1.add contact\n2.show contact\n3.edit contact\n4.delete\n5.exit");
 			int choice = s.nextInt();
-			if (choice == 5)
-				break;
-
 			switch (choice) {
 			case 1:
-				addressOperation.addContact(addressBooks.selectAddressBook(addressBookName).addressBook);
+				addressBooks.addAddressBooks();
 				break;
 			case 2:
-				addressOperation.showContacts(addressBooks.selectAddressBook(addressBookName).addressBook);
+				System.out.println("enter Contact book name");
+				addressBookName = s.next();
+				if (addressBooks.selectAddressBook(addressBookName) != null)
+					contactsMenu(addressBooks);
 				break;
 			case 3:
-				addressOperation.editContact(addressBooks.selectAddressBook(addressBookName).addressBook);
+				addressBooks.showAddressBook();
 				break;
 			case 4:
-				addressOperation.deleteContact(addressBooks.selectAddressBook(addressBookName).addressBook);
+				System.out.println("enter first name");
+				String firstName=s.next();
+				System.out.println("press 1 to search by state or 2 to search by city");
+				int searchChoice=s.nextInt();
+				if(searchChoice==1) {
+					System.out.println("enter state name");
+					String state=s.next();
+					addressBooks.searchByState(firstName, state);
+				}
+				else if(searchChoice==2) {
+					System.out.println("enter city name");
+					String city=s.next();
+					addressBooks.searchByCity(firstName, city);
+				}
 				break;
+			default:
+				return;
 			}
+		}
 
+	}
+
+	public static void contactsMenu(AddressBookArray addressBooks) {
+		AddressBookImpl addressOperation = new AddressBookImpl();
+		Scanner s = new Scanner(System.in);
+		System.out.println("1.add contact\n2.show contact\n3.edit contact\n4.delete\n5.exit");
+		int choice = s.nextInt();
+
+		switch (choice) {
+		case 1:
+			addressOperation.addContact(addressBooks.selectAddressBook(addressBookName).addressBook);
+			break;
+		case 2:
+			addressOperation.showContacts(addressBooks.selectAddressBook(addressBookName).addressBook);
+			break;
+		case 3:
+			addressOperation.editContact(addressBooks.selectAddressBook(addressBookName).addressBook);
+			break;
+		case 4:
+			addressOperation.deleteContact(addressBooks.selectAddressBook(addressBookName).addressBook);
+			break;
+		default:
+			return;
 		}
 	}
 }

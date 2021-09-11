4cf5a48899efc7e4e84180e6103fd21ec1b69b60
Vinay288 <vinayhiremath288@gmail.com>
2021-09-11 19:59:51 +0530

    MODIFIED: parameters of methods to hashmap of addressbook

--- src/AddressBookIf.java
+++ src/AddressBookIf.java
@@ -1,9 +1,10 @@
+import java.util.HashMap;
 
 public interface AddressBookIf {
 
-	public void addContact();
-	public void showContacts();
-	public void editContact();
-	public void deleteContact();
+	public void addContact(HashMap<String, Contact> contactHashMap);
+	public void showContacts(HashMap<String, Contact> contactHashMap);
+	public void editContact(HashMap<String, Contact> contactHashMap);
+	public void deleteContact(HashMap<String, Contact> contactHashMap);
 
 }

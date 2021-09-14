8fd5e984618313cb6d7bf702a03223928a2d66a1
Vinay288 <vinayhiremath288@gmail.com>
2021-09-14 15:25:47 +0530

    BUGFIX: resolved error in search method

--- src/AddressBookArray.java
+++ src/AddressBookArray.java
@@ -1,5 +1,4 @@
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,11 +9,11 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class AddressBookArray {
-	
+
 	private AddressBook addressBook;
 	private Map<String, AddressBook> addressBooksArray;
 	Scanner scanner = new Scanner(System.in);
-	
+
 	AddressBookArray() {
 		addressBooksArray = new HashMap<String, AddressBook>();
 	}
@@ -37,7 +36,7 @@ public class AddressBookArray {
 		}
 	}
 
-	public AddressBook selectAddressBook(String name) {	
+	public AddressBook selectAddressBook(String name) {
 		if (addressBooksArray.containsKey(name)) {
 			addressBook = addressBooksArray.get(name);
 			return addressBook;
@@ -45,32 +44,17 @@ public class AddressBookArray {
 		System.out.println("no such contact book");
 		return null;
 	}
-	
+
 	public void searchByState(String name, String state) {
-//		for(AddressBook addressBook:addressBooksArray.values()) {
-//			for(Contact contact:addressBook.getContact()) {
-//				if(contact.getFirstName().equals(name) && contact.getState().equals(state)) {
-//					System.out.println(name+" is found ");
-//					System.out.println(contact);
-//				}
-//			}
-//		}
-		Predicate<Contact> isEqual=c->((c.getFirstName().equals(name))&&(c.getState().equals(state)));
-		List<Contact> listtArrayList= addressBooksArray.values().stream()
-										.flatMap(contactsArray->addressBook.getContact().stream()
-										.filter(isEqual)).peek(n->System.out.println(name+"is found and details are \n"+n)).collect(Collectors.toList());
-										
-//	System.out.println(listtArrayList);
+		Predicate<Contact> isEqual = c -> ((c.getFirstName().equals(name)) && (c.getState().equals(state)));
+		addressBooksArray.values().stream().flatMap(contactsArray -> addressBook.getContact().stream().filter(isEqual))
+				.forEach(n -> System.out.println(name + " is found and details are \n" + n));
+
 	}
-	
+
 	public void searchByCity(String name, String city) {
-		for(AddressBook addressBook:addressBooksArray.values()) {
-			for(Contact contact:addressBook.getContact()) {
-				if(contact.getFirstName().equals(name) && contact.getCity().equals(city)) {
-					System.out.println(name+" is found ");
-					System.out.println(contact);
-				}
-			}
-		}
+		Predicate<Contact> isEqual = c -> ((c.getFirstName().equals(name)) && (c.getState().equals(city)));
+		addressBooksArray.values().stream().flatMap(contactsArray -> addressBook.getContact().stream().filter(isEqual))
+				.forEach(n -> System.out.println(name + " is found and details are \n" + n));
 	}
 }

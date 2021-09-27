be97b281ccaaff3ef58b21e6b540631c7e379a6b
Vinay288 <vinayhiremath288@gmail.com>
2021-09-28 00:45:33 +0530

    ADDED: used prepared statements to read contacts

--- src/AddressBookDBService.java
+++ src/AddressBookDBService.java
@@ -6,24 +6,43 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class AddressBookDBService {
+	private static AddressBookDBService addressBookDBService;
+	private java.sql.PreparedStatement readContactPreparedStatement;
+	private AddressBookDBService() {
+		
+	}
+	public static AddressBookDBService getDBInstance() {
+		addressBookDBService = new AddressBookDBService();
+		return addressBookDBService;
+	}
 	
 public List<Contact> readContacts(String addressBookName){
-	String sql="SELECT first_name,last_name,city,address,state,zip,phone_number,email from address_book a,contact_details c,addressbook_contact ac,place p  where p.place_id=c.place_id and a.address_book_id=ac.address_book_id and c.contact_id=ac.contact_id and address_book_name=\""+addressBookName+"\"";
+	ResultSet resultSet;
+	if(readContactPreparedStatement==null)
+		this.preparedStatementToReadContacts();
+	try {
+		readContactPreparedStatement.setString(1, addressBookName);
+		resultSet=readContactPreparedStatement.executeQuery();
+	} catch (SQLException e) {
+		throw new DBException(e.getMessage());
+	}
+	
+	return getContactList(resultSet);
+}
+public List<Contact> getContactList(ResultSet resultSet){
 	List<Contact> contactsList=new ArrayList<Contact>();
 	try {
-		Connection connection=this.getConnection();
-		System.out.println("connected");
-		Statement statement=connection.createStatement();
-		ResultSet resultSet=statement.executeQuery(sql);
 		while(resultSet.next()) {
 			contactsList.add(new Contact(resultSet.getString("first_name"), resultSet.getString("last_name"), resultSet.getString("city"), resultSet.getString("address"), resultSet.getString("state"), resultSet.getInt("zip"),new Long(resultSet.getLong("phone_number")).intValue(), resultSet.getString("email")));
 		}
-		
-	}catch (Exception e) {
-		e.printStackTrace();
+	} catch (SQLException e) {
+		throw new DBException(e.getMessage());
 	}
 	return contactsList;
+
 }
 public void writeAddressBookDB(Contact contact,String addressBookName) {
 	String insertQueryString="INSERT INTO contact_details values("+contact.getContactId()+",\""+contact.getFirstName()+"\",\""+contact.getLastName()+"\",\""+contact.getAddress()+"\",\""+contact.getEmail()+"\","+contact.getPlace().getPlaceId()+",\""+contact.getPhoneNumber()+"\")";
@@ -50,6 +69,15 @@ private Connection getConnection() throws SQLException, ClassNotFoundException {
 	String password="1234";
 	Connection connection=DriverManager.getConnection(jdbcURL,userName,password);
 	return connection;
+}
+private void preparedStatementToReadContacts() {
+    try {
+        Connection connection = this.getConnection();
+    	String sql="SELECT first_name,last_name,city,address,state,zip,phone_number,email from address_book a,contact_details c,addressbook_contact ac,place p  where p.place_id=c.place_id and a.address_book_id=ac.address_book_id and c.contact_id=ac.contact_id and address_book_name=?";
+       readContactPreparedStatement=connection.prepareStatement(sql);
+    } catch (Exception e) {
+        throw new DBException(e.getMessage());
+    }
 }
 
 }

39fd3970502c4a41f796c74c96b2ea50e1d0630e
Vinay288 <vinayhiremath288@gmail.com>
2021-09-28 23:58:02 +0530

    MODIFIED: test to check if inserted contact is in sync

--- src/AddressBookSystemTest.java
+++ src/AddressBookSystemTest.java
@@ -4,24 +4,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class AddressBookSystemTest {
-@Test
-public void givenAddressBookName_CheckIfAllContactsAreFetched(){
-	AddressBookImpl addressBookImpl=new AddressBookImpl();
-	int AddressBookSize=addressBookImpl.readDb("address_book1");
-	Assert.assertEquals(3,	AddressBookSize);
-}
+	@Test
+	public void givenAddressBookName_CheckIfAllContactsAreFetched() {
+		AddressBookImpl addressBookImpl = new AddressBookImpl();
+		List<Contact> contacts = addressBookImpl.readDb("address_book1");
+		Assert.assertEquals(3, contacts.size());
+	}
 
-@Test
-public void insertContactToGivenAddressBook_checkIfInserted() {
-	AddressBookImpl addressBookImpl=new AddressBookImpl();
-	int AddressBookSize=addressBookImpl.readDb("address_book3");
-	Assert.assertEquals(2,	AddressBookSize);
-	System.out.println(AddressBookSize);
-	Contact contact= new Contact(123, "qwerty", "hii", "mangalore", "hampankatta", "karnataks",5457, 875454,"adada", 46);
-	addressBookImpl.writeAddressBookDB(contact,"address_book1");
-	int updatedSize=addressBookImpl.readDb("address_book3");
-	System.out.println(updatedSize);
-	Assert.assertEquals(AddressBookSize+1,updatedSize);
-	
-}
+	@Test
+	public void insertContactToGivenAddressBook_checkIfInserted() {
+		AddressBookImpl addressBookImpl = new AddressBookImpl();
+		Contact contact = new Contact(125, "qwerty", "hii", "mangalore", "hampankatta", "karnataks", 5457, 875454,
+				"adada", 55);
+		Contact updatedContact = addressBookImpl.writeAddressBookDB(contact, "address_book1");
+		List<Contact> employeeList = addressBookImpl.readDb("address_book1");
+		Boolean result = addressBookImpl.compareContactSync(updatedContact, "address_book1");
+		Assert.assertTrue(result);
+
+	}
 }
